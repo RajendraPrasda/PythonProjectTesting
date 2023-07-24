b6f7687f0c55013fa392d59d798f065abc5145df
RajendraPrasda <rajendra.p@celestialsys.com>
2023-07-24 19:43:45 +0530

    Initial commit

---
+++ ListDatatypes.py
@@ -0,0 +1,16 @@
+''' List Is Mutable.
+Mutable -  We can change the existing behavior.
+List is data type that allows multiple values and can be different data types.'''
+
+values = [1, 2, "Raju", 4, 6]
+print(values)
+print(values[0])  #1
+print(values[-1])  #6
+print(values[0:2]) # 1, 2, "Raju"
+
+'''values.instert(3, 'ram')   # [1, 2, "Raju", "ram",  4, 6]
+print(values)
+values.append("end") #[1, 2, "Raju", "ram",  4, 6, "end"]
+values[2] = "ravi"  #updating
+del values[0]
+print(values)'''
---
+++ PythonDatatypes.py
@@ -0,0 +1,19 @@
+#Data Type  : Numeric , String, List, Tuple, Dictionary.
+#Numeric : Int, long, Float, complex.
+Test = 10
+print(Test)
+
+Str = "Welcome to python"
+print(Str)
+print(Str[0])
+print(Str[0:6])
+print(Str[-1])
+print(Str[::-1])
+
+a, b, c = 5, 12.5, "Welcome"
+#print("Value is"+b)  this concatenate str is not support in python.
+print("{} {} {}".format("Value is ", b, c))
+
+print(type(a))
+print(type(b))
+print(type(c))
